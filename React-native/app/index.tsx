e19977b7411aab9b3765e9652c851edcc8ba6551
Lauryne <mmelolotte2002@gmail.com>
2025-03-11 12:07:49 +0100

    ajout Paper

--- React-native/app/index.tsx
+++ React-native/app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button mode="contained" onPress={() => console.log("Bouton pressé")}>
+        Appuie-moi !
+      </Button>
     </View>
   );
 }
